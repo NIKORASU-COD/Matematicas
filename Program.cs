17b0677ad1ac3ed7bb0a3a2f3b7a66c5bd07ed3b
NicolasB <nicolasgamer908@gmail.com>
2025-03-18 20:47:40 -0500

    implementacion cateorias angulos y más

--- Program.cs
+++ Program.cs
@@ -1,52 +1,69 @@
 ﻿
-Console.WriteLine("");
-Console.WriteLine("--- BIENVENIDOS A NUESTRO PROYECTO ---");
-Console.WriteLine("");
+Console.WriteLine("\n--- BIENVENIDOS A NUESTRO PROYECTO ---");
+Console.WriteLine("\n-- Para calcular los datos de tu triangulo --");
+Console.WriteLine("\n- Por favor ingresa el lado #1");
+double ladoA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\n- Ahora el lado #2");
+double ladoB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\n- Finalmente el lado #3");
+double ladoC = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\n-----------------------------");
 
-Console.WriteLine("-- Para calcular los datos de tu triangulo --");
-Console.WriteLine("");
-Console.WriteLine("- Por favor ingresa el lado #1");
-Console.WriteLine("");
-int ladoA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("- Ahora el lado #2");
-Console.WriteLine("");
-int ladoB = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("- Finalmente el lado #3");
-Console.WriteLine("");
-int ladoC = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("-----------------------------");
+if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA)
 
-if (ladoA == ladoB && ladoB == ladoC)
+    if (ladoA == ladoB && ladoB == ladoC)
+    {
+        Console.WriteLine("\nTu triangulito es Equilatero");
+    
+        Console.WriteLine("\n   /\\");
+        Console.WriteLine("  /  \\");
+        Console.WriteLine(" /    \\");
+        Console.WriteLine("/______\\");
+        Console.WriteLine($"\nPorque todos sus lados son iguales ( lado 1 = {ladoA}, lado 2 = {ladoB} y lado 3 = {ladoC} )");
+    }
+
+        else if (ladoA != ladoB && ladoB == ladoC ||ladoB != ladoA && ladoA == ladoC || ladoC != ladoA && ladoA == ladoB)
+        {
+            Console.WriteLine("\nTu triangulito es Isosceles");
+            Console.WriteLine("");
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+            Console.WriteLine("");
+        }
+
+        else if (ladoA != ladoB && ladoB != ladoC)
+        {
+            Console.WriteLine("\nTu triangulito es Escaleno");
+            Console.WriteLine("");
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+            Console.WriteLine("");
+        }
+    else{}
+
+else
 {
-    Console.WriteLine("Tu triangulito es Equilatero");
-    Console.WriteLine("");
-    Console.WriteLine("   /\\");
-    Console.WriteLine("  /  \\");
-    Console.WriteLine(" /    \\");
-    Console.WriteLine("/______\\");
-    Console.WriteLine("");
-    Console.WriteLine($"Porque todos sus lados son iguales ( lado 1 = {ladoA}, lado 2 = {ladoB} y lado 3 = {ladoC} )");
+    Console.WriteLine("Los lados ingresados no forman un triangulo válido");
 }
 
-    else if (ladoA != ladoB && ladoB == ladoC ||ladoB != ladoA && ladoA == ladoC || ladoC != ladoA && ladoA == ladoB)
-    {
-        Console.WriteLine("Tu triangulito es Isosceles");
-        Console.WriteLine("");
-        Console.WriteLine("   /\\");
-        Console.WriteLine("  /  \\");
-        Console.WriteLine(" /    \\");
-        Console.WriteLine("/______\\");
-        Console.WriteLine("");
-    }
+// double anguloA = 5;
+// double anguloB = 5;
+// double anguloC = 2;
 
-    else if (ladoA != ladoB && ladoB != ladoC)
-    {
-        Console.WriteLine("Tu triangulito es Escaleno");
-        Console.WriteLine("");
-        Console.WriteLine("   /\\");
-        Console.WriteLine("  /  \\");
-        Console.WriteLine(" /    \\");
-        Console.WriteLine("/______\\");
-        Console.WriteLine("");
-    }
-else{}
+// if (anguloA == 90 || anguloB == 90 || anguloC == 90)
+// {
+//     Console.WriteLine("Tu triangulo es Rectangulo");
+// }
+//     else if (anguloA < 90 && anguloB < 90 && anguloC <90)
+//     {
+//         Console.WriteLine("Tu triangulo es Acotangulo");
+//     }
+//     else if (anguloA > 90 || anguloB > 90 || anguloC > 90)
+//     {
+//         Console.WriteLine("Tu triangulo es Obtusangulo");
+//     }
+// else{}
